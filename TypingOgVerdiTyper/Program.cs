d739c8c0c3f0119a0b360cddd4869de31f02dd0b
jensboy97 <141591601+jensboy97@users.noreply.github.com>
2023-11-20 16:59:48 +0100

    Add files via upload

---
+++ TypingOgVerdiTyper/Program.cs
@@ -0,0 +1,27 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System.Diagnostics.CodeAnalysis;
+
+    
+
+int siffer = 3;
+long lang = -9;
+float flyt = -3f;
+decimal desimal = 1.99m;
+double dobbel = 2.2;
+string tekst = "HelloWorld";
+char jej = 'J';
+bool nei = false;
+Console.WriteLine($" {siffer} " +
+                  $" {lang} " +
+                  $" {flyt}  " +
+                  $" {desimal}  " +
+                  $" {dobbel}  " +
+                  $" {tekst}  " +
+                  $" {jej}  " +
+                  $" {nei}");
+
+int a = 5;
+decimal b = 3.0m;
+decimal sum = a + b; 
+Console.WriteLine(sum);
