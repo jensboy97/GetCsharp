0b25f2ffba37f6477179d66f443399b1efa602c6
jensboy97 <141591601+jensboy97@users.noreply.github.com>
2023-11-21 13:47:42 +0100

    Add files via upload

---
+++ variabeltilordning/variabeltilordning/Class1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace variabeltilordning
+{
+    internal class Datatyper
+    {
+        int Tall1 = 2;
+        int Tall2 = 4;
+        string nullverdi = "denne motoden returnerer ingenting";
+
+        public int ReturnWholeNumber(int nr1, int nr2)
+        {
+
+            return nr1 + nr2;
+        }
+
+        public string ReturnString(string tekst)
+        {
+            return tekst; 
+        }
+
+        public void run()
+        {
+            Console.WriteLine($"dette er en int: {ReturnWholeNumber(Tall1, Tall2)}");
+            Console.WriteLine($"dette er en string {ReturnString(nullverdi)} ");
+        }
+    }
+}
---
+++ variabeltilordning/variabeltilordning/Program.cs
@@ -0,0 +1,16 @@
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace variabeltilordning
+{
+    internal class Program
+    {
+       static void Main(string[] args)
+       {
+           var Datatyper = new Datatyper(); 
+               Datatyper.run();
+        }
+
+      
+
+    }
+}
