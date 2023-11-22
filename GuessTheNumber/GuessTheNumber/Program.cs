0c7673f128837b7b15ab5f90e2f7a93c24348057
jensboy97 <141591601+jensboy97@users.noreply.github.com>
2023-11-22 14:19:14 +0100

    Add files via upload

---
+++ GuessTheNumber/GuessTheNumber/Program.cs
@@ -0,0 +1,34 @@
+﻿// See https://aka.ms/new-console-template for more information
+namespace GuessTheNumber;
+   
+class Guess
+    {
+        static void Main()
+        {
+            guessGame();
+        }
+
+        static void guessGame()
+        {
+            Console.WriteLine("skriv et tall mellom 0 og 100");
+            int answer = Convert.ToInt32(Console.ReadLine());
+            Random rand = new Random();
+            int randomNumber = rand.Next(1, 101);
+
+            while (answer != randomNumber)
+            {
+                string text = (answer < randomNumber) ? "Tallet er høyere!" : "Tallet er lavere!";
+                Console.WriteLine(text);
+                answer = Convert.ToInt32(Console.ReadLine());
+            }
+
+            if (answer == randomNumber)
+            {
+                Console.WriteLine("Riktig!");
+                Console.WriteLine("trykk enter for å spille igjen");
+                Console.ReadLine();
+                guessGame();
+
+            }
+        }
+    }
