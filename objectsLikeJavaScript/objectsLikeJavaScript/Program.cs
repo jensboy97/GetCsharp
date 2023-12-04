8ccb43f49e2352a9f0a3771f8e34f6293a425b34
jensboy97 <141591601+jensboy97@users.noreply.github.com>
2023-12-04 13:22:11 +0100

    Add files via upload

---
+++ objectsLikeJavaScript/objectsLikeJavaScript/Program.cs
@@ -0,0 +1,19 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+namespace objectsLikeJavaScript
+
+
+{
+    
+    public class LikeJs
+    {
+        public static void Main()
+        {
+            var pikachu = new Pokemon("Pikachu", 50, 21);
+            var charmander = new Pokemon("Charmander", 62, 15);
+            Console.WriteLine($"Name: {pikachu.Name} Health:{pikachu.Health} Level:{pikachu.Level}");
+            Console.WriteLine($"Name: {charmander.Name} Health:{charmander.Health} Level:{charmander.Level}");
+            
+        }
+    }
+}
---
+++ objectsLikeJavaScript/objectsLikeJavaScript/pikachu.cs
@@ -0,0 +1,19 @@
+namespace objectsLikeJavaScript;
+
+
+
+internal class Pokemon
+{
+    public string Name;
+    public int Health;
+    public int Level;
+
+    public Pokemon(string name, int health, int level )
+    {
+        Name = name;
+        Health = health; 
+        Level = level; 
+        
+
+    }
+}
